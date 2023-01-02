5d3936e5a1bb5d9f25948369b42eed569466a5f5
Parag koche <60149545+Paragkoche@users.noreply.github.com>
2023-01-02 07:35:50 +0000

    update

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+const express = require("express");
+const { Mailchain } = require("@mailchain/sdk");
+var cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const secretRecoveryPhrase =
+  "catalog useless rifle have jelly season unlock alert blood sound cause state cousin dawn they shiver pattern actress spray climb know spike cat kid"; // 25 word mnemonicPhrase
+app.get("/text", (r, q) => q.send("ok"));
+app.post("/", async (req, res) => {
+  const json = req.body;
+  const mailchain = Mailchain.fromSecretRecoveryPhrase(secretRecoveryPhrase);
+  const user = await mailchain.user();
+  const r = await mailchain.sendMail({
+    from: user.address,
+    to: [`vrcclinic@mailchain.com`],
+    subject: "New Appointment",
+    content: {
+      text: "New Appointment",
+      html: `
+        <h1>Name</h1>
+        <p>${json.fist_name} ${json.last_name}</p>
+        <h1>Date of Birth</h1>
+        <p>${json.dob}</p>
+        <h1>Contact details</h1>
+        <div>
+        <p>phone Number : <a herf="tel://${json.phone_number}">${json.phone_number}</a>
+        <p>address : <address>${json.address}</address>
+        </div>
+        <h1>what happened??</h1>
+        <p>${json.happened}</p>
+        
+        `,
+    },
+  });
+  res.json(r.status);
+});
 app.listen(process.env.PORT || 3000)
